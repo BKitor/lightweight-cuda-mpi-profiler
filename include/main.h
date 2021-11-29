1955fffa1dfe5261873099c541fa6df5ef23d47f
bkitor <bkitor@gmail.com>
2021-11-29 18:07:29 -0500

    data is tracked in mempool managed linked list

--- include/main.h
+++ include/main.h
@@ -24,27 +24,68 @@
 #include "cuda_helpers.h"
 #include "mpi_helpers.h"
 
-#define LARGE_BSIZE (1<<22) // 4MB
+ // should be around 30MB x 4proc/node for 1 million entries
+#define LWCMPP_POOL_SIZE (1 << 20)
+#define LWCMPP_OUT_DIR "./lwcmpp_output"
+#define LWCMPP_OUT_FILE "./lwcmpp_output/lwcmpp_out_rank%d.txt"
 
 // MPI functions we plan to profile
-int MPI_Init(int *argc, char ***argv);
-int MPI_Init_thread(int *argc, char ***argv, int required, int *provided);
+int MPI_Init(int* argc, char*** argv);
+int MPI_Init_thread(int* argc, char*** argv, int required, int* provided);
 int MPI_Finalize(void);
 
 // Collectives
-int MPI_Allreduce(const void *sendbuf, void *recvbuf, int count,
-                  MPI_Datatype datatype, MPI_Op op, MPI_Comm comm);
-int MPI_Bcast(void *buffer, int count,
-                  MPI_Datatype datatype, int root, MPI_Comm comm);
+int MPI_Allreduce(const void* sendbuf, void* recvbuf, int count,
+	MPI_Datatype datatype, MPI_Op op, MPI_Comm comm);
+int MPI_Bcast(void* buffer, int count,
+	MPI_Datatype datatype, int root, MPI_Comm comm);
 
-// Metrics we will profile
-#define COLL_COUNT_MAX 32 
-typedef struct lwcmp_data_t {
-  double msize_times[COLL_COUNT_MAX];
-  int64_t msize_counts[COLL_COUNT_MAX];
-} lwcmp_data_t;
+double lwcmpp_start_time = -1;
+// sizeof = 28B
+typedef struct lwcmpp_data_node_t {
+	struct lwcmpp_data_node_t* next;
+	double arrival_time;
+	double exit_time;
+	int32_t msize;
+} lwcmpp_data_node_t;
 
-lwcmp_data_t lwcmp_data;
-lwcmp_data_t lwcmp_data_cuda;
+lwcmpp_data_node_t* lwcmpp_data_head = NULL;
+lwcmpp_data_node_t* lwcmpp_data_tail = NULL;
+lwcmpp_data_node_t* lwcmpp_data_head_gpu = NULL;
+lwcmpp_data_node_t* lwcmpp_data_tail_gpu = NULL;
+
+typedef struct  lwcmpp_pool_t {
+	struct lwcmpp_pool_t* next;
+	lwcmpp_data_node_t* pool;
+	int32_t size;
+	int32_t idx;
+} lwcmpp_pool_t;
+
+lwcmpp_pool_t* lwcmpp_pool_head = NULL;
+lwcmpp_pool_t* lwcmpp_pool_tail = NULL;
+int lwcmpp_pool_init();
+void lwcmpp_pool_destroy();
+
+static inline void lwcmpp_pool_extend() {
+	lwcmpp_pool_t *p = calloc(1, sizeof(lwcmpp_pool_t));
+	p->pool = calloc(LWCMPP_POOL_SIZE, sizeof(lwcmpp_data_node_t));
+	p->next = NULL;
+	p->size = LWCMPP_POOL_SIZE;
+	p->idx = 0;
+	lwcmpp_pool_tail->next = p;
+	lwcmpp_pool_tail = p;
+    printf("alloc pool with size %d and idx %d\n", p->size, p->idx);
+	fflush(stdout);
+}
+
+static inline lwcmpp_data_node_t* lwcmpp_pool_alloc_node() {
+	if (__builtin_expect(lwcmpp_pool_tail->idx > lwcmpp_pool_tail->size, 0)) {
+		lwcmpp_pool_extend();
+	}
+
+	lwcmpp_data_node_t* ret = &lwcmpp_pool_tail->pool[lwcmpp_pool_tail->idx];
+	lwcmpp_pool_tail->idx++;
+	return ret;
+}
 
 #endif // MAIN_H
--- src/main.c
+++ src/main.c
@@ -16,96 +16,149 @@
  */
 
 #include "main.h"
+#include <sys/stat.h>
+
+int lwcmpp_pool_init() {
+  lwcmpp_pool_head = calloc(1, sizeof(lwcmpp_pool_t));
+  if (NULL == lwcmpp_pool_head) {
+    return -1;
+  }
+
+  lwcmpp_pool_tail = lwcmpp_pool_head;
+  lwcmpp_pool_head->next = NULL;
+  lwcmpp_pool_head->size = LWCMPP_POOL_SIZE;
+  lwcmpp_pool_head->idx = 0;
+  lwcmpp_pool_head->pool = calloc(LWCMPP_POOL_SIZE, sizeof(lwcmpp_data_node_t));
+  if (NULL == lwcmpp_pool_head->pool) {
+    return -1;
+  }
+
+  return 0;
+}
+
+void lwcmpp_pool_destroy() {
+  lwcmpp_pool_t* pool = lwcmpp_pool_head;
+  lwcmpp_pool_t* tmp = NULL;
+  while (NULL != pool) {
+    free(pool->pool);
+    tmp = pool->next;
+    free(pool);
+    pool = tmp;
+  }
+}
 
 static inline void init_metrics() {
-  for(int32_t i = 0; i<COLL_COUNT_MAX; i++){
-    lwcmp_data.msize_counts[i] = 0;
-    lwcmp_data.msize_times[i] = 0.0;
-    lwcmp_data_cuda.msize_counts[i] = 0;
-    lwcmp_data_cuda.msize_times[i] = 0.0;
-  }
+  // Should probably handle llwcmpp_pool_init's return value
+  lwcmpp_pool_init();
+
+  lwcmpp_data_node_t* node_cpu = lwcmpp_pool_alloc_node();
+  node_cpu->arrival_time = -1;
+  node_cpu->exit_time = -1;
+  node_cpu->msize = -1;
+  lwcmpp_data_head = node_cpu;
+  lwcmpp_data_tail = node_cpu;
+
+  lwcmpp_data_node_t* node_gpu = lwcmpp_pool_alloc_node();
+  node_gpu->arrival_time = -1;
+  node_gpu->exit_time = -1;
+  node_gpu->msize = -1;
+  lwcmpp_data_head_gpu = node_gpu;
+  lwcmpp_data_tail_gpu = node_gpu;
 }
 
-static inline void count_metric_ar(int count, MPI_Datatype datatype, double time, lwcmp_data_t *data) {
+static inline void count_metric_ar(int count, MPI_Datatype datatype, double ar_time, double ex_time, lwcmpp_data_node_t** tail) {
   int32_t msize = get_MPI_message_size(datatype, count);
-  
-  int idx = __builtin_clz(msize);
-  idx = 32 - idx;
-  data->msize_counts[idx]++;
-  data->msize_times[idx]+= time;
+
+  lwcmpp_data_node_t* node = lwcmpp_pool_alloc_node();
+  (*tail)->next = node;
+  node->arrival_time = ar_time;
+  node->exit_time = ex_time;
+  node->msize = msize;
+  (*tail) = node;
 }
 
-static inline void print_metrics(lwcmp_data_t * d, int32_t size) {
-  char *suffix[] = {"B", "KB", "MB", "GB", "TB"};
-  char length = sizeof(suffix) / sizeof(suffix[0]);
-  static char output[256];
-
-  for(int64_t msize = 0; msize<COLL_COUNT_MAX; msize++){
-    int32_t suf_idx = 0;
-    int64_t bytes = ((int64_t)1)<<msize;
-    bytes >>= 1;
-    int64_t dblBytes = bytes;
-
-    if (bytes > 1024) {
-      for (suf_idx = 0; (bytes / 1024) > 0 && suf_idx<length-1; suf_idx++, bytes /= 1024)
-        dblBytes = bytes / 1024.0;
-    }
-
-    printf("%ld%s\tcount:%-*ld time:%.2f us\n", 
-      dblBytes, suffix[suf_idx],
-      10, d->msize_counts[msize]/size, 
-      d->msize_times[msize]*1e6/(double)size);
-  }
-}
-
-static inline void cleanup_metrics(double f_time){
+static inline void cleanup_metrics(double end_time) {
   int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  if (rank != 0){
-    PMPI_Reduce(lwcmp_data.msize_times, NULL, 32, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    PMPI_Reduce(lwcmp_data.msize_counts, NULL, 32, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-    PMPI_Reduce(lwcmp_data_cuda.msize_times, NULL, 32, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    PMPI_Reduce(lwcmp_data_cuda.msize_counts, NULL, 32, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-  }else{
-    PMPI_Reduce(MPI_IN_PLACE, lwcmp_data.msize_times, 32, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    PMPI_Reduce(MPI_IN_PLACE, lwcmp_data.msize_counts, 32, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-    PMPI_Reduce(MPI_IN_PLACE, lwcmp_data_cuda.msize_times, 32, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    PMPI_Reduce(MPI_IN_PLACE, lwcmp_data_cuda.msize_counts, 32, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+  lwcmpp_data_node_t* n = lwcmpp_data_head->next;
+  lwcmpp_data_node_t* n_gpu = lwcmpp_data_head_gpu->next;
+  
+  if(rank==0){
+    struct stat st = {0};
+    if(stat(LWCMPP_OUT_DIR, &st) == -1){
+      mkdir(LWCMPP_OUT_DIR, 0777);
+    }
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+  char f_out_name[256];
+  sprintf(f_out_name, LWCMPP_OUT_FILE, rank);
+  FILE *f_out = fopen(f_out_name, "w");
+  if (NULL == f_out){
+    fprintf(stderr, "rank %d could't open file %s\n", rank, f_out_name);
+    goto lwcmpp_cleanup_abort;
+  }
 
-    printf("LWMP CPU METRICS:\n");
-    print_metrics(&lwcmp_data, size);
-    printf("LWMP GPU METRICS:\n");
-    print_metrics(&lwcmp_data_cuda, size);
+  fprintf(f_out,"CPU Allareduce times:\n");
+  fflush(f_out);
+  int count = 0;
+  while (n != NULL) {
+    count++;
+    fprintf(f_out,"%d\t%.6f\t%.6f\t%d\n", rank, n->arrival_time, n->exit_time, n->msize);
+
+    n = n->next;
+  }
+
+  fprintf(f_out,"GPU Allareduce times:\n");
+  fflush(f_out);
+  int count_gpu = 0;
+  while (n_gpu != NULL) {
+    count_gpu++;
+    fprintf(f_out,"GPU: %d\t%.6f\t%.6f\t%d\n", rank, n_gpu->arrival_time, n_gpu->exit_time, n_gpu->msize);
+    n_gpu = n_gpu->next;
   }
   
+  fflush(stdout);
+  PMPI_Barrier(MPI_COMM_WORLD);
+
+  fprintf(stdout, "MPI_Rank %d, of %d, ran for: %.6f counted %d ar, %d gpu ar\n", rank, size, end_time, count, count_gpu);
+  fprintf(f_out, "MPI_Rank %d, of %d, ran for: %.6f counted %d ar, %d gpu ar\n", rank, size, end_time, count, count_gpu);
+
+  fclose(f_out);
+
+  lwcmpp_cleanup_abort:
+  lwcmpp_pool_destroy();
 }
 
-int MPI_Init(int *argc, char ***argv) {
+int MPI_Init(int* argc, char*** argv) {
   init_metrics();
-  return PMPI_Init(argc, argv);
+  int ret = PMPI_Init(argc, argv);
+  lwcmpp_start_time = MPI_Wtime();
+  return ret;
 }
 
-int MPI_Init_thread(int *argc, char ***argv, int required, int *provided) {
+int MPI_Init_thread(int* argc, char*** argv, int required, int* provided) {
   init_metrics();
-  return PMPI_Init_thread(argc, argv, required, provided);
+  int ret = PMPI_Init_thread(argc, argv, required, provided);
+  lwcmpp_start_time = MPI_Wtime();
+  return ret;
 }
 
-int MPI_Allreduce(const void *sendbuf, void *recvbuf, int count,
-                  MPI_Datatype datatype, MPI_Op op, MPI_Comm comm) {
+int MPI_Allreduce(const void* sendbuf, void* recvbuf, int count,
+  MPI_Datatype datatype, MPI_Op op, MPI_Comm comm) {
   double s_time, e_time;
   int ret;
-  lwcmp_data_t *d = &lwcmp_data;
+  lwcmpp_data_node_t** d = &lwcmpp_data_tail;
 
   s_time = MPI_Wtime();
   ret = PMPI_Allreduce(sendbuf, recvbuf, count, datatype, op, comm);
   e_time = MPI_Wtime();
-  
-  if(is_device_pointer(recvbuf))
-    d = &lwcmp_data_cuda;
 
-  count_metric_ar(count, datatype, e_time - s_time, d);
+  if (is_device_pointer(recvbuf))
+    d = &lwcmpp_data_tail_gpu;
+
+  count_metric_ar(count, datatype, s_time, e_time, d);
 
   return ret;
 }
